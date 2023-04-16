4a8bbd8b5a31a50448b4300baae2f7211b76f12c
ming-Jo <mwinterj@daum.net>
2023-04-16 21:50:18 +0900

    🔨 Chore : 기본 font family 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "main-choco": "#8C5637",
         "light-choco": "#ffebd9",
+        "dark-gray": "#767676",
+      },
+      fontFamily: {
+        spoqa: ["Spoqa Han Sans Neo", "sans-serif"],
       },
     },
   },
