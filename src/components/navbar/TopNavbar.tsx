c69123aeaa07341cc6ba54ec28fe3aa1d4621124
ming-Jo <mwinterj@daum.net>
2023-04-16 22:37:52 +0900

    🎨 Design : 상단 Navbar 컴포넌트 생성 및 스타일 추가

--- src/components/navbar/TopNavbar.tsx
+++ src/components/navbar/TopNavbar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Logo from "./Logo";
+import SearchInput from "./SearchInput";
+import LoginButton from "./LoginButton";
+import ShoppingCartButton from "./ShoppingCartButton";
 
 const TopNavbar = () => {
   return (
-    <div>TopNavbar</div>
-  )
-}
+    <div className="flex justify-between items-center px-16 shadow-[0_4px_5px_rgba(0,0,0,0.1)]">
+      <Logo />
+      <SearchInput />
+      <div className="flex gap-[2.6rem] shrink-0">
+        <ShoppingCartButton />
+        <LoginButton />
+      </div>
+    </div>
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
