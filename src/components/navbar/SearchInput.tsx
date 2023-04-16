cf8b7e617e76d3a92318af315dbd356cd4ab5c0e
ming-Jo <mwinterj@daum.net>
2023-04-16 21:55:30 +0900

    🏷 Markup : search 아이콘 asset 추가 및 SearchInput 컴포넌트 생성

---
+++ src/components/navbar/SearchInput.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <form className="my-[2.2rem]">
+      <fieldset className="flex justify-between items-center w-[40rem] px-[2.2rem] py-[1rem] bg-light-choco rounded-full border-2 border-main-choco">
+        <legend className="sr-only">상품 검색</legend>
+        <label htmlFor="searchInp" className="sr-only"></label>
+        <input
+          type="text"
+          id="searchInp"
+          placeholder="상품을 검색해보세요!"
+          className="text-[1.6rem] bg-transparent"
+        />
+        <button
+          type="button"
+          className="w-[2.8rem] h-[2.8rem] bg-[url('src/assets/icon-search.svg')] bg-no-repeat bg-center bg-cover"
+        ></button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default SearchInput;
