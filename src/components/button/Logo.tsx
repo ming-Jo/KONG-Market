01b28a97aff15db48b8c3caa77fc0564a7690dec
ming-Jo <mwinterj@daum.net>
2023-04-23 01:11:35 +0900

    🔨 Chore : Logo 컴포넌트 재사용을 위해 size를 union type으로 지정

--- src/components/button/Logo.tsx
+++ src/components/button/Logo.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import logo from "@assets/logo-kong.svg";
 
-const Logo = () => {
+interface LogoProps {
+  logoSize: "small" | "big";
+}
+
+const Logo = ({ ...props }: LogoProps) => {
   return (
-    <h1 className="shrink-0">
-      <img src={logo} alt="KONG Market 로고 이미지" />
+    <h1
+      className={`${
+        props.logoSize === "big" ? "w-[24rem] mx-auto mt-[10rem] mb-[7rem]" : ""
+      } shrink-0`}
+    >
+      <img src={logo} alt="KONG Market 로고 이미지" className="w-full" />
     </h1>
   );
 };
