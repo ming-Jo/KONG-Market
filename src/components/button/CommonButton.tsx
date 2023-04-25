a1199de6733445571da01caeb0eff2693c94280a
ming-Jo <mwinterj@daum.net>
2023-04-25 21:34:46 +0900

    🏷 Markup : 공통 컴포넌트 LoginTapButton 생성 및 CommonButton 조건부 스타일 적용 #1

--- src/components/button/CommonButton.tsx
+++ src/components/button/CommonButton.tsx
@@ -3,15 +3,18 @@ import React from "react";
 interface CommonBtnProps {
   children: React.ReactNode;
   type?: "button" | "submit";
-  width?: string;
-  padding?: string;
-  fontSize?: string;
   icon?: string;
+  className?: string;
 }
 
 const CommonButton = ({ children, ...props }: CommonBtnProps) => {
   return (
-    <button {...props} className="rounded-lg">
+    <button
+      {...props}
+      className={`px-[1rem] py-[1.8rem] rounded-[0.5rem] bg-main-choco text-white ${
+        props.className || ""
+      }`}
+    >
       {props.icon && <img src={props.icon} />}
       {children}
     </button>
---
+++ src/components/button/LoginTapButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface LoginTapBtnProps {
+  children: React.ReactNode;
+  active?: boolean;
+}
+
+const LoginTapButton = ({ ...props }: LoginTapBtnProps) => {
+  return (
+    <button
+      type="button"
+      className={`${
+        props.active
+          ? "btn-login-tap before:content-[''] before:absolute before:-bottom-[1.6rem] before:left-0 before:bg-white before:block before:w-full before:h-[2rem] before:px-[2rem] before:border before:border-white"
+          : "btn-login-tap bg-[#f1ece8]"
+      }`}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default LoginTapButton;
