f4afd12441120de91e6f166cb08939102c44668a
ming-Jo <mwinterj@daum.net>
2023-04-25 21:40:09 +0900

    🏷 Markup : LoginForm 레이아웃 구성 #3

--- src/components/input/LoginForm.tsx
+++ src/components/input/LoginForm.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-
-interface LoginInputProps {
-  type?: string;
-}
+import LoginTapButton from "@components/button/LoginTapButton";
+import CommonButton from "@components/button/CommonButton";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   return (
-    <section>
+    <section className="flex flex-col mx-auto w-[55rem] relative">
       <h2 className="sr-only">로그인 화면</h2>
-      <div>
-        <button type="button" className="btn-login-tap">
-          구매회원 로그인
-        </button>
-        <button type="button" className="btn-login-tap">
-          판매회원 로그인
-        </button>
+      <div className="flex text-main-choco font-medium">
+        <LoginTapButton active={true}>구매회원 로그인</LoginTapButton>
+        <LoginTapButton active={false}>판매회원 로그인</LoginTapButton>
       </div>
       <form>
-        <fieldset className="flex flex-col">
+        <fieldset className="flex flex-col rounded-b-[1rem] border-r border-l border-b border-main-choco">
           <legend className="sr-only">login form</legend>
           <label htmlFor="userId" className="sr-only">
             아이디
@@ -37,8 +32,18 @@ const LoginForm = () => {
             placeholder="비밀번호를 입력하세요"
             className="input-underline"
           />
+          <CommonButton
+            type="button"
+            className="w-[48rem] my-[3.6rem] text-[1.8rem] mx-auto"
+          >
+            로그인
+          </CommonButton>
         </fieldset>
       </form>
+      <div className="flex items-center gap-10 mx-auto mt-12 text-[1.6rem] relative before:inline-block before:w-[0.1rem] before:h-[1.6rem] before:absolute before:left-[7.2rem] before:bg-current">
+        <Link to="/signup">회원가입</Link>
+        <Link to="/signup">비밀번호 찾기</Link>
+      </div>
     </section>
   );
 };
