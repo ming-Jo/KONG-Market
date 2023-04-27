5e44c6924271c3ccef669ce3981831c910c574b2
ming-Jo <mwinterj@daum.net>
2023-04-28 00:36:52 +0900

    🏷 Markup : input 공통 컴포넌트 생성 #1

--- src/components/input/CommonInput.tsx
+++ src/components/input/CommonInput.tsx
@@ -7,17 +7,66 @@ interface CommonInputProps {
   className?: string;
 }
 
-const CommonInput = ({ ...props }: CommonInputProps) => {
+interface CommonLabelInputProps {
+  type: string;
+  children?: React.ReactNode;
+  id?: string;
+  placeholder?: string;
+  autocomplete?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+}
+
+interface InvalidSpanProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const CommonInput = ({ ...props }: CommonInputProps) => {
   return (
     <input
       type={props.type}
       id={props.id}
       placeholder={props.placeholder}
-      className={`p-[1.6rem] border border-dark-gray rounded-[0.5rem] text-[1.6rem] focus:outline-main-choco ${
+      className={`peer p-[1.6rem] border border-dark-gray rounded-[0.5rem] text-[1.6rem] invalid:outline-red-400 valid:outline-green-300 ${
         props.className || ""
       }`}
     />
   );
 };
 
-export default CommonInput;
+export const CommonLabelInput = ({ ...props }: CommonLabelInputProps) => {
+  return (
+    <>
+      <label
+        htmlFor={props.id}
+        className={`mt-5 mb-4 text-[1.6rem] text-dark-gray ${
+          props.labelClassName || ""
+        }`}
+      >
+        {props.children}
+      </label>
+      <input
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+        autoComplete={props.autocomplete}
+        className={`peer p-[1.6rem] border border-dark-gray rounded-[0.5rem] text-[1.6rem]  invalid:outline-red-400 valid:outline-green-300 ${
+          props.inputClassName || ""
+        }`}
+      />
+    </>
+  );
+};
+
+export const InvalidSpan = ({ children, ...props }: InvalidSpanProps) => {
+  return (
+    <span
+      className={`hidden peer-invalid:block mt-2 text-red-400 ${
+        props.className || ""
+      }`}
+    >
+      {children}
+    </span>
+  );
+};
