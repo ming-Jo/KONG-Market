3475cf9047298575d1124630d9237a9ce2b16f0b
ming-Jo <mwinterj@daum.net>
2023-04-30 21:51:54 +0900

    🔨 Chore : tailwind config 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "main-choco": "#8C5637",
         "light-choco": "#ffebd9",
         "dark-gray": "#767676",
+        "disabled-gray": "#C4C4C4",
         current: "currentColor",
       },
       fontFamily: {
