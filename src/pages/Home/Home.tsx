379c357b7fee2ef725d92046033a07bf5175dd20
ming-Jo <mwinterj@daum.net>
2023-05-12 00:54:02 +0900

    🎨 Design : Home 페이지에 Carousel 기본 레이아웃 적용 #3

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Carousel from "@components/carousel/Carousel";
 import ProductListItem from "@components/contents/ProductListItem";
 
 const Home = () => {
   return (
     <div className="max-w-[138rem] mx-auto">
+      <Carousel />
       <section className="max-w-[120rem] px-16 py-32 mx-auto">
         <h2 className="sr-only">판매 상품 리스트</h2>
         <ul className="grid grid-cols-3 gap-x-[5vw] gap-y-[5vh] justify-items-center">
