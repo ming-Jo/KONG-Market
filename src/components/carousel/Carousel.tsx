ae646ab01fe13b6de21efdefcdcee8b7a7903060
ming-Jo <mwinterj@daum.net>
2023-05-12 00:52:50 +0900

    🏷 Markup : Carousel 컴포넌트 생성 #3

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import iconSwiperLeft from "@assets/icon-swiper-1.svg";
+import iconSwiperRight from "@assets/icon-swiper-2.svg";
+
+const Carousel = () => {
+  return (
+    <article className="h-[50rem] bg-yellow-50 relative">
+      <h2 className="sr-only">메인 홈 배너 캐러셀</h2>
+      <button
+        type="button"
+        className="absolute left-0 h-full top-1/2 -translate-y-2/4"
+      >
+        <img src={iconSwiperLeft} alt="" />
+      </button>
+      <button
+        type="button"
+        className="absolute right-0 h-full top-1/2 -translate-y-2/4"
+      >
+        <img src={iconSwiperRight} alt="" />
+      </button>
+      <div className="absolute flex gap-4 bottom-10 left-1/2 -translate-x-2/4">
+        <button
+          type="button"
+          className="w-4 h-4 bg-current rounded-full"
+        ></button>
+        <button
+          type="button"
+          className="w-4 h-4 bg-current rounded-full"
+        ></button>
+        <button
+          type="button"
+          className="w-4 h-4 bg-current rounded-full"
+        ></button>
+        <button
+          type="button"
+          className="w-4 h-4 bg-current rounded-full"
+        ></button>
+      </div>
+    </article>
+  );
+};
+
+export default Carousel;
