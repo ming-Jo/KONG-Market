2085dab8d7645dc12fc32b8a2745a773b4815339
ming-Jo <mwinterj@daum.net>
2023-05-11 23:51:10 +0900

    🏷 Markup : Home 화면에 ProductListItem 레이아웃 적용 #3

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import ProductListItem from "@components/contents/ProductListItem";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="max-w-[138rem] mx-auto">
+      <section className="max-w-[120rem] px-16 py-32 mx-auto">
+        <h2 className="sr-only">판매 상품 리스트</h2>
+        <ul className="grid grid-cols-3 gap-x-[5vw] gap-y-[5vh] justify-items-center">
+          <ProductListItem />
+          <ProductListItem />
+          <ProductListItem />
+          <ProductListItem />
+          <ProductListItem />
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
