17d12c7342e9abf05e3b614faa2881277dbe486d
ming-Jo <mwinterj@daum.net>
2023-05-18 22:48:00 +0900

    ✨ Feat :  Detail Tab 클릭 시 동적 text 렌더링 구현

--- src/components/details/DetailTab.tsx
+++ src/components/details/DetailTab.tsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { DetailMenuButton } from "@components/button/CommonButton";
 
 const DetailTab = () => {
+  const [selectText, setSelectText] = useState("제품 상세");
+
+  const handleSelectTab = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (event.currentTarget.textContent !== null) {
+      setSelectText(event.currentTarget.textContent);
+    }
+  };
+
   return (
     <div className="col-[span_2/auto] mt-[10rem]">
       <ul className="grid grid-cols-4 text-[1.8rem]">
         <li>
-          <DetailMenuButton>제품 상세</DetailMenuButton>
+          <DetailMenuButton
+            clicked={selectText === "제품 상세" ? true : false}
+            clickHandler={handleSelectTab}
+          >
+            제품 상세
+          </DetailMenuButton>
         </li>
         <li>
-          <DetailMenuButton>리뷰</DetailMenuButton>
+          <DetailMenuButton
+            clicked={selectText === "리뷰" ? true : false}
+            clickHandler={handleSelectTab}
+          >
+            리뷰
+          </DetailMenuButton>
         </li>
         <li>
-          <DetailMenuButton>Q&A</DetailMenuButton>
+          <DetailMenuButton
+            clicked={selectText === "Q&A" ? true : false}
+            clickHandler={handleSelectTab}
+          >
+            Q&A
+          </DetailMenuButton>
         </li>
         <li>
-          <DetailMenuButton>반품/교환정보</DetailMenuButton>
+          <DetailMenuButton
+            clicked={selectText === "반품/교환정보" ? true : false}
+            clickHandler={handleSelectTab}
+          >
+            반품/교환정보
+          </DetailMenuButton>
         </li>
       </ul>
-      <div></div>
+      <div className="mt-20 bg-light-gray h-[45rem] text-disabled-gray text-5xl font-medium text-center leading-[45rem]">
+        {selectText}
+      </div>
     </div>
   );
 };
