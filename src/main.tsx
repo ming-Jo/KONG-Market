4d842159f0aacf0b630c2cad2e042d6e8e1b76aa
minji.ji <mwinterj@daum.net>
2023-06-18 11:00:12 +0900

    🔨 Chore : 프로젝트 전체에 Redux-Toolkit 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import { router } from "@routes/Routes";
+import store from "@/store/store";
 import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
