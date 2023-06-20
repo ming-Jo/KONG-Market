0e04a39618387c2016e9834aeec785b73f3064ad
minji.ji <mwinterj@daum.net>
2023-06-20 17:17:36 +0900

    🏷 Markup : 마이페이지 버튼에 생성될 ArrowModal 컴포넌트 생성
    - #3 #5 #7
    - Modal의 상태를 전역으로 관리하여 로직 관리 예정

---
+++ src/components/modal/ArrowModal.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ArrowModalProps {
+  list: { value: string; onClick: () => void }[];
+}
+
+const ArrowModal = ({ list }: ArrowModalProps) => {
+  return (
+    <div className="absolute text-center bg-white border rounded-2xl w-fit top-[7.5rem] -right-[3rem] border-disabled-gray text-dark-gray shadow-md before:absolute before:w-0 before:h-0 before:border-l-[1.2rem] before:border-r-[1.2rem] before:border-b-[1.2rem] before:border-t-0 before:border-transparent before:border-b-disabled-gray before:-top-[1.2rem] before:right-[4.5rem]">
+      <div className="p-5 before:absolute before:w-0 before:h-0 before:border-l-[1.2rem] before:border-r-[1.2rem] before:border-b-[1.2rem] before:border-t-0 before:border-transparent before:border-b-white before:-top-[1.05rem] before:right-[4.5rem]">
+        {list.map((item, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              onClick={item.onClick}
+              className="w-full py-4 border border-white rounded-2xl hover:border hover:border-main-choco hover:text-main-choco hover:bg-light-choco"
+            >
+              {item.value}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ArrowModal;
