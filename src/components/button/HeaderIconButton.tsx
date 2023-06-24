b0dd52566598d5b203765589a3ea8baab6a7a3a7
minji.ji <mwinterj@daum.net>
2023-06-24 11:56:25 +0900

    ♻️ Refactor : img태그를 svg 컴포넌트로 변경 및 props 타입 추가
    - #3
    - 헤더 아이콘버튼을 active 되었을 때 동적으로 컬러를 변경될 수 있도록 수정

--- src/components/button/HeaderIconButton.tsx
+++ src/components/button/HeaderIconButton.tsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 
 interface HeaderIconBtnProps {
   children: React.ReactNode;
-  iconSrc: string;
+  svg: React.FC<React.SVGProps<SVGSVGElement>>;
+  stroke?: string;
   to: string;
   color: string;
 }
 
-const HeaderIconButton = ({ ...props }: HeaderIconBtnProps) => {
+const HeaderIconButton = ({
+  svg: SvgComponent,
+  ...props
+}: HeaderIconBtnProps) => {
   return (
     <Link to={props.to} className={`inline-block text-[1.2rem] ${props.color}`}>
-      <img src={props.iconSrc} alt="" className="w-full h-[3.2rem] mb-2" />
+      <SvgComponent stroke={props.stroke} className="w-full h-[3.2rem] mb-2" />
       {props.children}
     </Link>
   );
