7c80fe0593ff91308ab95572eed03b5ea57fe863
minji.ji <mwinterj@daum.net>
2023-07-17 22:48:18 +0900

    🏷 Markup : HomeSection 컴포넌트 분리 및 디테일 스타일 수정
    - #3 #5 #8
    - 컴포넌트 가독성을 위해 분리해서 관리

--- src/components/contents/ProductListItem.tsx
+++ src/components/contents/ProductListItem.tsx
@@ -1,14 +1,14 @@
-import React, { Children } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Product } from "@store/slice/productSlice";
 
 const ProductListItem = ({ product }: { product: Product }) => {
   return (
-    <li className="w-full">
+    <li className="w-full p-5 bg-white shadow-small rounded-3xl hover:bg-light-choco">
       <Link to={`product/${product.product_id}`}>
         <figure>
           <div
-            className={`relative mb-8 border border-dark-gray rounded-2xl overflow-hidden h-0 pt-[100%]`}
+            className={`relative mb-6 border border-light-gray rounded-2xl overflow-hidden h-0 pt-[100%]`}
           >
             <img
               src={`${product ? product.image : ""}`}
@@ -16,10 +16,10 @@ const ProductListItem = ({ product }: { product: Product }) => {
               className="absolute top-0 left-0 w-full h-full"
             />
           </div>
-          <p className="text-dark-gray">{product.store_name}</p>
-          <figcaption className="text-[1.8rem] mt-4">
+          <p className="px-4 text-dark-gray">{product.store_name}</p>
+          <figcaption className="text-[1.8rem] mt-4 px-4">
             {product.product_name}
-            <strong className="block text-[2.4rem] mt-4">
+            <strong className="block text-[2.2rem] mt-4">
               {product.price.toLocaleString("ko-KR")}
               <span className="ml-2">원</span>
             </strong>
---
+++ src/pages/Home/HomeSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Product } from "@store/slice/productSlice";
+import ProductListItem from "@components/contents/ProductListItem";
+
+const HomeSection = ({ productList }: { productList: Product[] }) => {
+  return (
+    <section className="max-w-[120rem] px-16 py-32 mx-auto">
+      <h2 className="sr-only">판매 상품 리스트</h2>
+      <ul className="grid grid-cols-3 gap-x-[5vw] gap-y-[5vh] justify-items-center max-[900px]:grid-cols-2">
+        {productList.map((product) => (
+          <ProductListItem key={product.product_id} product={product} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default HomeSection;
