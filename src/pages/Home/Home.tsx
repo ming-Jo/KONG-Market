4c6519e95e494988e4e1fbf02b4fe0e0fc9da81d
minji.ji <mwinterj@daum.net>
2023-07-18 14:57:55 +0900

    🌱 Style : HomeSection 컴포넌트 분리에 따른 적용 및 세부 스타일 수정
    - #5 #8

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,29 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@hooks/hooks";
 import { fetchProductData, getProductState } from "@store/slice/productSlice";
 import Carousel from "@components/carousel/Carousel";
-import ProductListItem from "@components/contents/ProductListItem";
+import HomeSection from "@pages/Home/HomeSection";
 
 const Home = () => {
-  const dispatch = useAppDispatch();
-  const { status, productList, totalPage } = useAppSelector(getProductState);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    dispatch(fetchProductData(page));
-    // window.scrollTo(0, 0);
-  }, [page]);
+  const { productList } = useAppSelector(getProductState);
 
   return (
     <main className="max-w-[138rem] mx-auto">
       <Carousel />
-      <section className="max-w-[120rem] px-16 py-32 mx-auto">
-        <h2 className="sr-only">판매 상품 리스트</h2>
-        <ul className="grid grid-cols-3 gap-x-[5vw] gap-y-[5vh] justify-items-center">
-          {productList.map((product) => (
-            <ProductListItem key={product.product_id} product={product} />
-          ))}
-        </ul>
-      </section>
+      <HomeSection productList={productList} />
     </main>
   );
 };
--- src/pages/Home/HomeSection.tsx
+++ src/pages/Home/HomeSection.tsx
@@ -6,7 +6,7 @@ const HomeSection = ({ productList }: { productList: Product[] }) => {
   return (
     <section className="max-w-[120rem] px-16 py-32 mx-auto">
       <h2 className="sr-only">판매 상품 리스트</h2>
-      <ul className="grid grid-cols-3 gap-x-[5vw] gap-y-[5vh] justify-items-center max-[900px]:grid-cols-2">
+      <ul className="grid grid-cols-3 gap-x-[4vh] gap-y-[5vw] justify-items-center max-[900px]:grid-cols-2">
         {productList.map((product) => (
           <ProductListItem key={product.product_id} product={product} />
         ))}
