58f823c940d85743ebdf0285772d1fc71252f82f
minji.ji <mwinterj@daum.net>
2023-07-19 16:35:51 +0900

    🔨 Chore : Carousel Sample Data 추가
    - #11
    - 캐러셀 데이터 목록을 array로 관리

---
+++ src/components/carousel/carouselData.ts
@@ -0,0 +1,7 @@
+// carousel Sample data
+export const carouselData = [
+  { id: 1, title: "banner1" },
+  { id: 2, title: "banner2" },
+  { id: 3, title: "banner3" },
+  { id: 4, title: "banner4" },
+];
