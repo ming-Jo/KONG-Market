ebd7eaca60ab4749e2ea58b07e7220bccdc29f05
minji.ji <mwinterj@daum.net>
2023-07-19 17:17:46 +0900

    🌱 Style : Carousel 내부 슬라이드 버튼을 재사용 컴포넌트로 수정
    - #11

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import iconSwiperLeft from "@assets/icon-swiper-1.svg";
-import iconSwiperRight from "@assets/icon-swiper-2.svg";
+import {
+  LeftArrowButton,
+  RightArrowButton,
+} from "@components/button/ArrowButton";
 import DotButtons from "@components/carousel/DotButton";
 
 const Carousel = () => {
   return (
-    <article className="h-[50rem] bg-yellow-50 relative">
+    <article className="h-[50rem] bg-yellow-50 relative mt-[8.6rem]">
       <h2 className="sr-only">메인 홈 배너 캐러셀</h2>
-      <button
-        type="button"
-        className="absolute left-0 h-full top-1/2 -translate-y-2/4"
-      >
-        <img src={iconSwiperLeft} alt="" />
-      </button>
-      <button
-        type="button"
-        className="absolute right-0 h-full top-1/2 -translate-y-2/4"
-      >
-        <img src={iconSwiperRight} alt="" />
-      </button>
+      <LeftArrowButton className="absolute left-0 h-full top-1/2 -translate-y-2/4" />
+      <RightArrowButton className="absolute right-0 h-full top-1/2 -translate-y-2/4" />
       <div className="absolute flex gap-4 bottom-10 left-1/2 -translate-x-2/4">
         <DotButtons />
         <DotButtons />
