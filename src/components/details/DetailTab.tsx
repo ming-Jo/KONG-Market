3937b7e7105c04a66e90fdd34a9953fb364f6ff2
minji.ji <mwinterj@daum.net>
2023-07-27 01:11:52 +0900

    ♻️ Refactor : DetailTab 메뉴 생성 코드 가독성 개선
    - #12
    - 메뉴 데이터를 array로 관리하고 반복 코드를 map()으로 개선

--- src/components/details/DetailTab.tsx
+++ src/components/details/DetailTab.tsx
@@ -4,6 +4,8 @@ import { getDetailState } from '@store/slice/detailSlice';
 import { DetailMenuButton } from '@components/button/CommonButton';
 
 const DetailTab = () => {
+  const detailMenuList = ['제품 상세', '리뷰', 'Q&A', '반품/교환정보'];
+
   const { detail } = useAppSelector(getDetailState);
   const [selectText, setSelectText] = useState('제품 상세');
 
@@ -16,26 +18,13 @@ const DetailTab = () => {
   return (
     <div className="mt-[10rem]">
       <ul className="grid grid-cols-4 text-[1.8rem]">
-        <li>
-          <DetailMenuButton clicked={selectText === '제품 상세' ? true : false} clickHandler={handleSelectTab}>
-            제품 상세
-          </DetailMenuButton>
-        </li>
-        <li>
-          <DetailMenuButton clicked={selectText === '리뷰' ? true : false} clickHandler={handleSelectTab}>
-            리뷰
-          </DetailMenuButton>
-        </li>
-        <li>
-          <DetailMenuButton clicked={selectText === 'Q&A' ? true : false} clickHandler={handleSelectTab}>
-            Q&A
-          </DetailMenuButton>
-        </li>
-        <li>
-          <DetailMenuButton clicked={selectText === '반품/교환정보' ? true : false} clickHandler={handleSelectTab}>
-            반품/교환정보
-          </DetailMenuButton>
-        </li>
+        {detailMenuList.map((item, index) => (
+          <li key={index}>
+            <DetailMenuButton clicked={selectText === item ? true : false} clickHandler={handleSelectTab}>
+              {item}
+            </DetailMenuButton>
+          </li>
+        ))}
       </ul>
       <div className="flex flex-col justify-center items-center mt-20 bg-light-gray h-[45rem] text-disabled-gray text-5xl font-medium text-center h-[45rem]">
         {selectText}
