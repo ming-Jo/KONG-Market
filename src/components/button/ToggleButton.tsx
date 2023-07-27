febdfbc37b81ba13cccef8ae5d2172a90031f575
minji.ji <mwinterj@daum.net>
2023-07-27 16:26:11 +0900

    ✨ Feat : userType에 따른 ToggleButton 구현
    - #17
    - toolkit에 저장된 login, signup 각각의 userType 사용
    - 버튼 클릭 시 pathname에 따라 조건에 맞는 코드 실행

--- src/components/button/ToggleButton.tsx
+++ src/components/button/ToggleButton.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import LoginTapButton from './LoginTapButton';
+import { useLocation } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '@hooks/hooks';
+import { getLoginUserType, setLoginUserType } from '@store/slice/loginSlice';
+import { getSignupUserType, setSignupUserType } from '@store/slice/signupSlice';
+import UserTypeTapButton from '@components/button/UserTypeTapButton';
 
 const ToggleButton = () => {
+  const { pathname } = useLocation();
+  const dispatch = useAppDispatch();
+  const userType = useAppSelector(pathname.includes('login') ? getLoginUserType : getSignupUserType);
+
+  const handleToggleButton = (usertype: string) => {
+    if (usertype === 'BUYER') {
+      pathname.includes('login') ? dispatch(setLoginUserType(usertype)) : dispatch(setSignupUserType(usertype));
+    } else {
+      pathname.includes('login') ? dispatch(setLoginUserType(usertype)) : dispatch(setSignupUserType(usertype));
+    }
+  };
+
   return (
     <div className="flex font-medium text-main-choco">
-      <LoginTapButton active={true}>구매회원 로그인</LoginTapButton>
-      <LoginTapButton active={false}>판매회원 로그인</LoginTapButton>
+      <UserTypeTapButton active={userType === 'BUYER' && true} onclick={() => handleToggleButton('BUYER')}>
+        구매회원 {pathname.includes('login') ? '로그인' : '회원가입'}
+      </UserTypeTapButton>
+      <UserTypeTapButton active={userType === 'SELLER' && true} onclick={() => handleToggleButton('SELLER')}>
+        판매회원 {pathname.includes('login') ? '로그인' : '회원가입'}
+      </UserTypeTapButton>
     </div>
   );
 };
